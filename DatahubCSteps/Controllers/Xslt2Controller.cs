7cd599ac95b8bf60c5a3aa01d2c5ed754dd00cb8
Brian Keating <b@briankeating.net>
2017-01-17 00:22:07 +0000

    Sample x-ms-dynamic-schema

---
+++ DatahubCSteps/Controllers/Xslt2Controller.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+using DatahubCSteps.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Swashbuckle.Swagger.Annotations;
+using TRex.Metadata;
+
+namespace DatahubCSteps.Controllers
+{
+    public class Xslt2Controller : ApiController
+    {
+
+        //// GET api/values/5
+        [System.Web.Http.HttpPost]
+        [SwaggerOperation("TransformToGDMX")]
+        [SwaggerResponse(HttpStatusCode.OK)]
+        [SwaggerResponse(HttpStatusCode.NotFound)]
+        public string Post(string input, string xsl, [System.Web.Http.FromBody]Payload xslParams)
+        {
+            return "value";
+        }
+
+        [System.Web.Http.HttpGet]
+        [System.Web.Http.Route("GetParams/{xsl}")]
+        [SwaggerOperation("GetParams")]
+        [SwaggerResponse(200, type: typeof(Columns))]
+        public IActionResult GetParams([FromRoute]string xsl)
+        {
+            string exampleJson = Properties.Resources.Example;
+
+            var example = exampleJson != null
+            ? JsonConvert.DeserializeObject<Columns>(exampleJson)
+            : default(Columns);
+            return new ObjectResult(example);
+        }
+
+        
+
+        
+
+
+    }
+}
---
+++ DatahubCSteps/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DatahubCSteps.WebApiApplication" Language="C#" %>
